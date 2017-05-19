bf7c71c829554d903ad69cb595e7161cfefe56fc
pengwan <wanpeng2008@gmail.com>
2017-05-19 15:48:53 +0800

    1

--- weChatMPWeb/src/app/pages/wechat-app/wechat-app.component.ts
+++ weChatMPWeb/src/app/pages/wechat-app/wechat-app.component.ts
@@ -22,6 +22,33 @@ export class WechatAppComponent implements OnInit {
   ngOnInit() {
     console.debug('WechatAppComponent init')
     this.settings = {
+      mode: external,
+      hideHeader: false,
+      hideSubHeader: false,
+      noDataMessage: '没有数据',
+      actions: {
+        columnTitle: '操作',
+        add: true,
+        edit: true,
+        delete: true,
+        position: 'left'
+      },
+      add: {
+        addButtonContent: '增加',
+        createButtonContent: '保存',
+        cancelButtonContent: '取消',
+        confirmCreate: true
+      },
+      edit: {
+        editButtonContent: '编辑',
+        saveButtonContent: '保存',
+        cancelButtonContent: '取消',
+        confirmSave: true,
+      },
+      delete:{
+        deleteButtonContent: '删除',
+        confirmDelete: true
+      },
       columns: {
         appId: {
           title: 'appId'
@@ -50,6 +77,10 @@ export class WechatAppComponent implements OnInit {
         createTime: {
           title: 'createTime'
         }
+      },
+      pager:  {
+        display:	true,
+        perPage:  20
       }
     };
     this.onSearch()
@@ -60,11 +91,30 @@ export class WechatAppComponent implements OnInit {
         this.wechatApps = res.content;
         console.debug(this.wechatApps)
         this.source.load(this.wechatApps);
+        this.source.setPaging(res.number,res.size)
       },
       error => {
 
       }
     )
+  }
+  onCreate(){
+    console.debug('onCreate invoked')
+  }
+  onCreateConfirm(){
+    console.debug('onCreateConfirm invoked')
+  }
+  onEdit(){
+    console.debug('onEdit invoked')
+  }
+  onEditConfirm(){
+    console.debug('onEditConfirm invoked')
+  }
+  onDelete(){
+    console.debug('onDelete invoked')
+  }
+  onDeleteConfirm(){
+    console.debug('onDeleteConfirm invoked')
   }
 
 }
