e6f7f3b416aa51031ed154ee8b61a64d52a4e5b9
pengwan <wanpeng2008@gmail.com>
2017-06-12 09:04:22 +0800

    1

--- AppParcelClient/src/app/share/directives/checklist.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ChecklistDirective } from './checklist.directive';
-
-describe('ChecklistDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ChecklistDirective();
-    expect(directive).toBeTruthy();
-  });
-});
